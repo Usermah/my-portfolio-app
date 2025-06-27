56a3823b1bc6888f7ce15e1b2cf94aa49f26a92a
Usermah <usamaibrahim737.com>
2025-06-27 10:00:45 -0500

    Initial push with Docker, CI/CD, and production config

---
+++ app/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+# Project Model
+class Project(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    image = models.ImageField(upload_to='project_images/', blank=True, null=True)
+    github_link = models.URLField(blank=True, null=True)
+    live_demo_link = models.URLField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+# Contact Message Model
+class ContactMessage(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    subject = models.CharField(max_length=200)
+    message = models.TextField()
+    sent_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name} - {self.subject}"
+
---
+++ app/views.py
@@ -0,0 +1,61 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import ContactForm
+from .models import Project
+
+import os
+from dotenv import load_dotenv
+import resend
+
+load_dotenv()
+resend.api_key = os.environ.get("RESEND_API_KEY")
+
+def home(request):
+    projects = Project.objects.all().order_by('-created_at')
+    resume = Project.objects.first()
+    return render(request, 'home.html', {'projects': projects, 'resume': resume})
+
+def contact(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+            name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            subject = form.cleaned_data['subject']
+            message = form.cleaned_data['message']
+
+            # Admin email content
+            full_message = f"""
+                <h3>New Message from Portfolio Contact Form</h3>
+                <p><strong>Name:</strong> {name}</p>
+                <p><strong>Email:</strong> {email}</p>
+                <p><strong>Message:</strong><br>{message}</p>
+            """
+
+            try:
+                # Only send to admin
+                resend.Emails.send({
+                    "from": "onboarding@resend.dev",
+                    "to": ["usamaibrahim737@gmail.com"],
+                    "subject": subject,
+                    "html": full_message
+                })
+
+                messages.success(request, "Message sent successfully!")
+
+            except Exception as e:
+                print("Email sending error:", e)
+                messages.error(request, "Failed to send email. Try again later.")
+
+            return redirect('contact')
+
+    else:
+        form = ContactForm()
+
+    return render(request, 'contact.html', {'form': form})
+
+
+def about(request):
+    return render(request, 'about.html')
