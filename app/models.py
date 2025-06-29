dc81d2214bd44448e38f6007d05e8485eaeac069
Usermah <usamaibrahim737.com>
2025-06-29 18:15:51 -0500

    Fixed

--- app/models.py
+++ app/models.py
@@ -1,15 +1,13 @@
 from django.db import models
 
-
-
-
+# Project Model
 class Project(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
+    image_name = models.CharField(max_length=200, blank=True, null=True) 
     github_link = models.URLField(blank=True, null=True)
     live_demo_link = models.URLField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    image_name = models.CharField(max_length=50, blank=True, null=True)
 
     def __str__(self):
         return self.title
